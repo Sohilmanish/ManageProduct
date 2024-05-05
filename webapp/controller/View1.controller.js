37db6e9feda94161df81cf020999022f84f49321
Manish khatik <manish.sohil1@gmail.com>
2024-05-05 05:40:27 +0000

    first commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,46 +10,44 @@ sap.ui.define([
 
         return Controller.extend("com.manish.manageproduct.controller.View1", {
             onInit: function () {
-             const that=this
-             const oDataV2Model =  this.getOwnerComponent().getModel()
-             oDataV2Model.read('/Products', {
-                success: function(odata, oResponse) {
-                    console.log('Data from service:', odata);
-                //     for(let i=0; i<odata.length; i++)
-                //     {
-                //      let pid=odata.results[i].ProductID ;
-                //     oDataV2Model.read("/Products("+pid+")/Supplier",{
-                //         success:function(data,oResponse){
-                //             console.log("here is my first supplier ", data)
-                //         },
-                //         error:function(error){
+            //  const that=this
+            //  const oDataV2Model =  this.getOwnerComponent().getModel()
+            //  oDataV2Model.read('/Products', {
+            //     success: function(odata, oResponse) {
+            //         console.log('Data from service:', odata);
+            //         for(let i=0; i<odata.length; i++)
+            //         {
+            //          let pid=odata.results[i].ProductID ;
+            //         oDataV2Model.read("/Products("+pid+")/Supplier",{
+            //             success:function(data,oResponse){
+            //                 console.log("here is my first supplier ", data)
+            //             },
+            //             error:function(error){
 
-                //         }
-                //     })
-                // }
-                    const oModel = new JSONModel(); 
-                    oModel.setData(odata.results); 
-                    that.getView().byId("idProductsTable").setModel(oModel, "P") 
-                    that.getView().byId("idProductsTable2").setModel(oModel, "P")
-                    that.getView().byId("idProductsTable3").setModel(oModel, "P")
-                    that.getView().byId("idProductsTable4").setModel(oModel, "P");
+            //             }
+            //         })
+            //     }
+            //         const oModel = new JSONModel(); 
+            //         oModel.setData(odata.results); 
+            //         that.getView().byId("idProductsTable").setModel(oModel, "P") 
+            //         that.getView().byId("idProductsTable2").setModel(oModel, "P")
+            //         that.getView().byId("idProductsTable3").setModel(oModel, "P")
+            //         that.getView().byId("idProductsTable4").setModel(oModel, "P");
 
-                },
-                error: function(oError) {
-                    console.log('Error in service call', oError)
+            //     },
+            //     error: function(oError) {
+            //         console.log('Error in service call', oError)
 
-                } 
-            }) 
+            //     } 
+            // }) 
             },
-
-            onPress:function(oEvent){
-                // var pId= oEvent.getSource().getBindingContext().getProperty("ProductID")
+ 
+                onPress:function(oEvent){
+                var pId= oEvent.getSource().getBindingContext().getProperty("ProductID")
                 var oRouter = this.getOwnerComponent().getRouter();
-                var index= oEvent.getSource().getBindingContext('P').sPath.split('/')[1]
-                var pId=oEvent.getSource().getModel('P').getData()[0].ProductID
-                oRouter.navTo("category", {
-                id: pId
-            });
+                // var index= oEvent.getSource().getBindingContext('P').sPath.split('/')[1]
+                // var pId=oEvent.getSource().getModel('P').getData()[0].ProductID
+                oRouter.navTo("category", {pId: pId});
             }
 
         });
--- webapp/controller/page2.controller.js
+++ webapp/controller/page2.controller.js
@@ -1,13 +1,17 @@
-sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/UIComponent",
+  "sap/ui/model/json/JSONModel"
+],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller,UIComponent, JSONModel) {
       "use strict";
   
       return BaseController.extend("com.manish.manageproduct.controller.page2", {
         onInit: function () {
-            UIComponent.getRouterFor(this).getRoute("RouteViewSupplier").attachPatternMatched(this.onObjectMatched, this);
+            UIComponent.getRouterFor(this).getRoute("category").attachPatternMatched(this.onObjectMatched, this);
             
         },
 
